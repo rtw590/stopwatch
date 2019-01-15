ec5ee340f71557cb6eda00a8401bedf3414149a8
Rob Waller <r.t.waller@gmail.com>
2019-01-15 12:20:10 -0600

    About and shortkeys modals working

---
+++ modals.js
@@ -0,0 +1,15 @@
+document.getElementById("closeAbout").addEventListener("click", function() {
+  document.getElementById("aboutBox").style.display = "none";
+});
+
+document.getElementById("about").addEventListener("click", function() {
+  document.getElementById("aboutBox").style.display = "inline";
+});
+
+document.getElementById("closeShortkeys").addEventListener("click", function() {
+  document.getElementById("shortkeysBox").style.display = "none";
+});
+
+document.getElementById("shortkeys").addEventListener("click", function() {
+  document.getElementById("shortkeysBox").style.display = "inline";
+});
